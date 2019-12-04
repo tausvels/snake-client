a5a11cd8a85e659a5034086abcfea4f90071e766
Tausif <engr.tausifkhan@gmail.com>
2019-12-04 23:30:35 +0000

    added the setup input

--- play.js
+++ play.js
@@ -1,10 +1,23 @@
 //const net = require('net');
-const connect = require('./client');
+const connect = require("./client");
 
 /**
  * Establishes connection with the game server
  */
-
-
-console.log('Connecting ...');
+console.log("Connecting ...");
 connect.connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function (key){
+    if(key === "\u0003"){
+      process.exit()
+    }
+  }
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+setupInput();
