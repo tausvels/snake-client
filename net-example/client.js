e97efccf81a41541b38111257c1dc390e6cc6475
Tausif <engr.tausifkhan@gmail.com>
2019-12-04 21:11:47 +0000

    modified the conenction function of play.js

--- net-example/client.js
+++ net-example/client.js
@@ -1,15 +1,15 @@
 const net = require('net');
 const conn = net.createConnection({
-  host: "localhost", // change to IP address
-  port: 8000
+  host: "192.168.88.173", // change to IP address
+  port: 3000
 });
 
 conn.setEncoding("utf8"); // interpret data as text
 
-conn.on("data", data => {
+conn.on("data", data => { // data received from SERVER
   console.log("Server says: ", data);
 });
 
-conn.on("connect", () => {
-  conn.write("Hello from client!");
+conn.on("connect", () => { // On CONNECTION, client sends to server.
+  conn.write("Hello from Tausif...HI!!!");
 });
--- net-example/server.js
+++ net-example/server.js
@@ -2,18 +2,24 @@ const net = require("net");
 
 const server = net.createServer();
 
-server.on("connection", client => {
+server.on("connection", client => { // client is the callback
   console.log("New client connected!");
   client.write("Hello there!");
 
   //connection callback
   client.setEncoding("utf8"); // interpret data as text
-  client.on("data", data => {
+  
+  client.on("data", data => { // data is the callback here which corresponds to 'Hello there'.
     console.log("Message from client: ", data);
   });
+
 });
 
-server.listen(8000, () => {
+server.on("close", end =>{
+  console.log("Connection terminated")
+})
+
+server.listen(3000, () => {
   //8000 works for me.
-  console.log("Server listening on port 8000!");
+  console.log("Server listening on port 3000!");
 });
